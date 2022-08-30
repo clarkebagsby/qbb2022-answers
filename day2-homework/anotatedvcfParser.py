44cef760f8e8da1a08fcc94d7a1bf01f375900e4
Clarke <cbagsby1@jhu.edu>
2022-08-30 17:51:57 -0400

    commented vcfParser.py

--- day2-homework/anotatedvcfParser.py
+++ day2-homework/anotatedvcfParser.py
@@ -3,26 +3,26 @@
 import sys
 
 def parse_vcf(fname):
-    vcf = []
-    info_description = {}
-    info_type = {}
-    format_description = {}
+    vcf = [] #will hold the entire file
+    info_description = {} #info about headers and sections
+    info_type = {} # for a certain field in info, going to tell us the type of info
+    format_description = {} # stores genotype formats
     type_map = {
         "Float": float,
         "Integer": int,
         "String": str
-        }
-    malformed = 0
+        } #key = datatyppe, value= is what is being converted
+    malformed = 0 # startiing counter
 
     try:
-        fs = open(fname)
+        fs = open(fname) #tryiing to open file name 
     except:
-        raise FileNotFoundError(f"{fname} does not appear to exist", file=sys.stderr)
+        raise FileNotFoundError(f"{fname} does not appear to exist", file=sys.stderr) #if the file is not found then will print error 
 
-    for h, line in enumerate(fs):
-        if line.startswith("#"):
-            try:
-                if line.startswith("##FORMAT"):
+    for h, line in enumerate(fs): #goes through each line in the file h is line #
+        if line.startswith("#"): #for the header, "if this is a header"
+            try: #formats the header 
+                if line.startswith("##FORMAT"): #see if its a format line
                     fields = line.split("=<")[1].rstrip(">\r\n") + ","
                     i = 0
                     start = 0
@@ -63,42 +63,43 @@ def parse_vcf(fname):
                     fields = line.lstrip("#").rstrip().split("\t")
                     vcf.append(fields)
             except:
-                raise RuntimeError("Malformed header")
-        else:
-            try:
-                fields = line.rstrip().split("\t")
-                fields[1] = int(fields[1])
-                if fields[5] != ".":
-                    fields[5] = float(fields[5])
-                info = {}
-                for entry in fields[7].split(";"):
-                    temp = entry.split("=")
-                    if len(temp) == 1:
-                        info[temp[0]] = None
-                    else:
-                        name, value = temp
-                        Type = info_type[name]
-                        info[name] = type_map[Type](value)
-                fields[7] = info
-                if len(fields) > 8:
-                    fields[8] = fields[8].split(":")
-                    if len(fields[8]) > 1:
-                        for i in range(9, len(fields)):
-                            fields[i] = fields[i].split(':')
-                    else:
-                        fields[8] = fields[8][0]
-                vcf.append(fields)
+                raise RuntimeError("Malformed header") #error mssg showed when headeer is incorrect
+        else: #looking at variants, the body 
+            try:# checking to see if variant sect is formatted correctly 
+                fields = line.rstrip().split("\t") #making a list of column info and splitting by tabs
+                fields[1] = int(fields[1]) #changing the position column(1) into int
+                if fields[5] != ".": # looks at qual column 
+                    fields[5] = float(fields[5]) # changes whatever is there into a float
+                info = {} #new dictionary 
+                for entry in fields[7].split(";"): #taking info str into a list, and lookiing at each str
+                    temp = entry.split("=") #a list of before equals and after equals
+                    if len(temp) == 1: #flag: no value associated, looks at each str to make sure that it is 0
+                        info[temp[0]] = None #will say none if temp is longer than 1
+                    else: #looks for those with equal 
+                        name, value = temp # name before equals, value after equals, sets variable to both those values
+                        Type = info_type[name] #looks through the info_type dict. to match datatype of name(previous )
+                        info[name] = type_map[Type](value) #pulls funct type_map, to change data_type in info dict.  
+                fields[7] = info #inserts dict. info for 
+                if len(fields) > 8: #the file does not have genetype info, so looking at those files 
+                    fields[8] = fields[8].split(":") #formatting for geneotype info, and replaces the 9th column, makes a list from those values
+                    if len(fields[8]) > 1: # looking at indexes of all the genotypes of people in the file, checking for the len of genotype info to be greater than 1
+                        for i in range(9, len(fields)): #starts at 1st genetype info( after 10th column)
+                            fields[i] = fields[i].split(':')#turning geneotype field into a list and replaces fields[i] with the new list
+                    else: 
+                        fields[8] = fields[8][0] #only 1 thing, store that as a list
+                vcf.append(fields) #sticking field list with formats into vcf
             except:
-                malformed += 1
-    vcf[0][7] = info_description
-    if len(vcf[0]) > 8:
-        vcf[0][8] = format_description
-    if malformed > 0:
-        print(f"There were {malformed} malformed entries", file=sys.stderr)
+                malformed += 1 
+   
+    vcf[0][7] = info_description #replaces 
+    if len(vcf[0]) > 8: #
+        vcf[0][8] = format_description #puts in header info.
+    if malformed > 0: #looks at if malformed has reached a value greater than 0, then prints statement and formats with malform
+        print(f"There were {malformed} malformed entries", file=sys.stderr) #takes malformed variable and 
     return vcf
 
-if __name__ == "__main__":
-    fname = sys.argv[1]
-    vcf = parse_vcf(fname)
-    for i in range(10):
+if __name__ == "__main__": 
+    fname = sys.argv[1] #whatever you give as a filename 
+    vcf = parse_vcf(fname) #running the file on the function created and storing output in vcf
+    for i in range(10): #prints first ten lines (lines 0-9)
         print(vcf[i])
