bcc57d6340d0c6f58e21b097f39b516ccaa4e37a
Clarke <cbagsby1@jhu.edu>
2022-08-30 09:25:44 -0400

    updated code for exercise3

--- day1-homework/exercise3.sh
+++ day1-homework/exercise3.sh
@@ -4,10 +4,10 @@
 
 awk '/^#/{next} BEGIN{OFS="\t"} {print $1,$2-1, $2}' $1 > variants.bed  #fixes tab in random_snippet 
 
-sort -k1,1 -k2,2n variants.bed > variants.sorted.bed
-sort -k1,1 -k2,2n ~/data/bed_files/genes.bed > genes.sorted.bed
+sort -k1,1 -k2,2n variants.bed > variants.sorted.bed #adds a sorted variants bed to be used for bedtools closest
+sort -k1,1 -k2,2n ~/data/bed_files/genes.bed > genes.sorted.bed #stays the same
 
-bedtools closest -a variants.sorted.bed -b genes.sorted.bed
+bedtools closest -a variants.sorted.bed -b genes.sorted.bed #prints variants for each gene at different chromosome #'s 
 
 
 
