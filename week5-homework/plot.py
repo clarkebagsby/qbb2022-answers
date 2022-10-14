7f899cab576ea15c07bcaa08db5f4f28148731f5
Clarke <cbagsby1@jhu.edu>
2022-10-14 13:01:18 -0400

    updated script

--- week5-homework/plot.py
+++ week5-homework/plot.py
@@ -13,14 +13,13 @@ R1_tc = load_data("R1_treat_scale_c.bdg")
 
 #outputs x and y in dict called data
 
-print(R1_tc)
 #step 5 graph
 fig, ax = plt.subplots(ncols= 1, nrows=4)
 ax[0].fill_between(D0_H3K27ac['X'], D0_H3K27ac["Y"])
 ax[1].fill_between(D2_H3K27ac['X'], D2_H3K27ac["Y"])
 ax[2].fill_between(R2_tc['X'], R2_tc["Y"])
 ax[3].fill_between(R1_tc['X'], R1_tc["Y"])
-plt.show()
+plt.savefig("wk5_a.pdf")
 
 
 
