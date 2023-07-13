4c9eece016a7e3382c6403b7533a01f9ae081b07
Clarke <cbagsby1@jhu.edu>
2023-07-13 12:02:00 -0400

    week10 hw edits

--- week10-homework/script.py
+++ week10-homework/script.py
@@ -4,16 +4,16 @@ adata = sc.read_10x_h5("neuron_10k_v3_filtered_feature_bc_matrix.h5")
 # Make variable names (in this case the genes) unique
 adata.var_names_make_unique()
 
-# no filter
-sc.tl.pca(adata)
-sc.pl.pca(adata)
-
-# #filter the adata by this recipe
-sc.pp.recipe_zheng17(adata)
-# # performing pca on the filtered data
-sc.tl.pca(adata)
-# # making a plot with the PCA adata
-sc.pl.pca(adata)
+# # no filter
+# sc.tl.pca(adata)
+# sc.pl.pca(adata)
+#
+# # #filter the adata by this recipe
+# sc.pp.recipe_zheng17(adata)
+# # # performing pca on the filtered data
+# sc.tl.pca(adata)
+# # # making a plot with the PCA adata
+# sc.pl.pca(adata)
 
 #making neighborhood graph
 sc.pp.neighbors(adata, n_neighbors=10, n_pcs=40)
@@ -21,21 +21,21 @@ sc.pp.neighbors(adata, n_neighbors=10, n_pcs=40)
 sc.tl.leiden(adata)
 
 sc.tl.tsne(adata)
-sc.pl.tsne(adata)
+# sc.pl.tsne(adata)
 
-sc.tl.umap(adata, maxiter=1000)
-sc.pl.umap(adata)
-#leiden gives a certain number of groups the genes fall inti 
-sc.tl.rank_genes_groups(adata, groupby='leiden', method='t-test')
-sc.pl.rank_genes_groups(adata)
-
-sc.tl.rank_genes_groups(adata, groupby='leiden', method='logreg')
-sc.pl.rank_genes_groups(adata)
+# sc.tl.umap(adata, maxiter=1000)
+# sc.pl.umap(adata)
+# #leiden gives a certain number of groups the genes fall inti
+# sc.tl.rank_genes_groups(adata, groupby='leiden', method='t-test')
+# sc.pl.rank_genes_groups(adata)
+#
+# sc.tl.rank_genes_groups(adata, groupby='leiden', method='logreg')
+# sc.pl.rank_genes_groups(adata)
 
 
 
 #support plot
-sc.pl.tsne(adata, color=['Hba-a1', 'Gad1', 'Gad2', 'Hes1', 'Pax6', 'Meg3'], legend_loc='right margin')
+sc.pl.tsne(adata, color=['Hba-a1', 'Gad1', 'Gad2', 'Pax6', 'Meg3'], legend_loc='right margin')
 sc.pl.tsne(adata, color='leiden', legend_loc='right margin')
 
 
